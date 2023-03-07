9954f38b0fcb18116b27c39ba80488e7b41594f7
alcho01 <74156727+alcho01@users.noreply.github.com>
2023-03-06 23:21:45 -0500

    Added Mob

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
+
 
 public class PlayerInventory : MonoBehaviour
 {
+    [SerializeField] TMP_Text LevelCompleteText;
+
     public int NumberOfPapers { get; private set; }
 
     public UnityEvent<PlayerInventory> OnPaperCollected;
@@ -12,7 +16,7 @@ public class PlayerInventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LevelCompleteText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,5 +29,16 @@ public class PlayerInventory : MonoBehaviour
     {
         NumberOfPapers++;
         OnPaperCollected.Invoke(this);
+
+        if (NumberOfPapers == 5)
+        {
+            GameComplete();
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void GameComplete()
+    {
+        LevelCompleteText.gameObject.SetActive(true);
     }
 }
